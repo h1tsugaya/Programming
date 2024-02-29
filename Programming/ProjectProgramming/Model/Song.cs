91f0e9622a1e634b77a8d1e429e8de1150953824
Сергей Масликов <smaslikova7240@gmail.com>
2024-02-29 22:34:15 +0700

    edit

--- Programming/ProjectProgramming/Model/Song.cs
+++ Programming/ProjectProgramming/Model/Song.cs
@@ -1,10 +1,78 @@
-﻿namespace ProjectProgramming.Model
+﻿using System;
+using System.Drawing;
+
+class Song
 {
-    public class Song
+    private string _title;
+    private string _artist;
+    private int _duration;
+    private int _release;
+
+
+    public string Title
     {
-        public string Title { get; set; }
-        public string Artist { get; set; }
-        public string Album { get; set; }
-        public string Genre { get; set; }
+        get { return _title; }
+        set
+        {
+            if (value.Length < 0)
+            {
+                throw new ArgumentException("Title is empty!");
+            }
+            _title = value;
+        }
+    }
+    public string Artist
+    {
+        get { return _artist; }
+        set
+        {
+            if (value.Length < 0)
+            {
+                throw new ArgumentException("Name of artist is empty!");
+            }
+            _artist = value;
+        }
+    }
+
+    public int Duration
+    {
+        get { return _duration; }
+        set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentException("The duration cannot be less than 1 minute!");
+            }
+            _duration = value;
+        }
+    }
+
+    public int Release
+    {
+        get { return _release; }
+        set
+        {
+            if (value < 1996)
+            {
+                throw new ArgumentException("There were no players before 1996!");
+            }
+            _release = value;
+        }
+    }
+
+    public Song()
+    {
+        Title = "Тп на аме";
+        Artist = "Серега Пират";
+        Duration = 155;
+        Release = 2021;
+    }
+
+    public Song(string title, string artist, int duration, int release)
+    {
+        Title = title;
+        Artist = artist;
+        Duration = duration;
+        Release = release;
     }
 }
