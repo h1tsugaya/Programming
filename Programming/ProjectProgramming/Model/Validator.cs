f3f991e30ab9349ac2d0991e5b11b4d881aad76c
Сергей Масликов <smaslikova7240@gmail.com>
2024-02-29 20:26:08 +0700

    создание класса Validator

---
+++ Programming/ProjectProgramming/Model/Validator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+static class Validator
+{
+    public static void AssertOnPositiveValue(int value)
+    {
+        if (value < 0) throw new ArgumentException($"The {new StackTrace().GetFrame(1).GetMethod().Name} property cannot be negative.");
+    }
+    public static void AssertOnPositiveValue(double value)
+    {
+        if (value < 0) throw new ArgumentException($"The {new StackTrace().GetFrame(1).GetMethod().Name} property cannot be negative.");
+    }
+
+    public static void AssertValueInRange(int value, int min, int max)
+    {
+        if (value < min || value >= max) throw new ArgumentException($"The value {new StackTrace().GetFrame(1).GetMethod().Name} is out of range.");
+    }
+    public static void AssertValueInRange(double value, double min, double max)
+    {
+        if (value < min || value >= max) throw new ArgumentException($"The value {new StackTrace().GetFrame(1).GetMethod().Name} is out of range.");
+    }
+}
