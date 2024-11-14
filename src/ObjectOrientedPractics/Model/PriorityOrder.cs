3909d7085b6fe8760d7678ca5a606906307afe42
Сергей Масликов <smaslikova7240@gmail.com>
2024-11-14 17:08:20 +0700

    создан класс PriorityOrder

---
+++ src/ObjectOrientedPractics/Model/PriorityOrder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PriorityOrder : Order
+    {
+        public DateTime DesiredDeliveryDate { get; set; }
+
+        public string DesiredDeliveryTime { get; set; }
+
+        public PriorityOrder() : base()
+        {
+            DesiredDeliveryDate = DateTime.Now;
+            DesiredDeliveryTime = "9:00 – 11:00";
+        }
+
+        public PriorityOrder(string customerName, OrderStatus status, DateTime creationDate, List<Item> items, DateTime desiredDeliveryDate, string desiredDeliveryTime)
+            : base(customerName, status, creationDate, items)
+        {
+            DesiredDeliveryDate = desiredDeliveryDate;
+            DesiredDeliveryTime = desiredDeliveryTime;
+        }
+    }
+}
